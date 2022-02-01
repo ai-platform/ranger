520f07fbc869f6b74ff05f3056b6b048e67e9b0f
Sharma <sharmaash876@gmail.com>
2022-02-01 11:17:42 +0530

    top nav changes

--- security-admin/src/main/webapp/scripts/views/common/Header.js
+++ security-admin/src/main/webapp/scripts/views/common/Header.js
@@ -21,25 +21,43 @@
 define(function(require){
     'use strict';
 
-	var Backbone		= require('backbone');
-	var Communicator	= require('communicator');
+	var Backbone            = require('backbone');
+    var XALinks             = require('modules/XALinks');
+    var XAEnums             = require('utils/XAEnums');
+    var XAUtil              = require('utils/XAUtils');
+    var SessionMgr          = require('mgrs/SessionMgr');
+    var App                 = require('App');
+    var localization        = require('utils/XALangSupport');
+    var RangerServiceList   = require('collections/RangerServiceList');
+    var RangerService       = require('models/RangerService');
+    var ServicemanagerSidebarlayoutTmpl = require('hbs!tmpl/common/ServiceManagerSidebarLayout_tmpl');
+    var vUploadServicePolicy     = require('views/UploadServicePolicy');
+    var vDownloadServicePolicy   = require('views/DownloadServicePolicy');
+    var RangerServiceViewDetail  = require('views/service/RangerServiceViewDetail');
+    var RangerServiceDefList    = require('collections/RangerServiceDefList');
+    var RangerServiceDef        = require('models/RangerServiceDef');
+    var RangerZoneList      = require('collections/RangerZoneList');
 
 	var Header_tmpl = require('hbs!tmpl/common/Header_tmpl'); 
 	
 	var Header = Backbone.Marionette.ItemView.extend(
-	/** @lends Footer */
+	/** @lends Header */
 	{
+
 		_viewName : Header,
 		
     	template: Header_tmpl,
         
     	/** ui selector cache */
-    	ui: {},
+    	ui: {
+            'sideCollapes' : '[data-id="sideCollapes"]',
+        },
 
 		/** ui events hash */
 		events: function() {
 			var events = {};
 			//events['change ' + this.ui.input]  = 'onInputChange';
+            events['mousedown ' + this.ui.sideCollapes] = 'sideCollapes';
 			return events;
 		},
 
@@ -51,7 +69,8 @@ define(function(require){
 			console.log("initialized a Header ItemView");
 
 			_.extend(this, _.pick(options, ''));
-
+            
+            this.collapes = false,
 			this.bindEvents();
 		},
 
@@ -65,6 +84,25 @@ define(function(require){
 		onRender: function() {
 			this.initializePlugins();
 		},
+
+        sideCollapes : function (e) {
+            e.stopImmediatePropagation()
+            if (this.collapes) {
+                console.log(this.collapes);
+                this.collapes = false;
+                App.rSideBar.$el.addClass('expanded');
+                App.rContent.$el.addClass('expanded-contant');
+                App.rSideBar.$el.removeClass('collapsed');
+                e.target.setAttribute('class' , 'fa fa-bars fa-lg')
+            } else {
+                console.log(this.collapes);
+                this.collapes = true;
+                App.rSideBar.$el.addClass('collapsed');
+                App.rContent.$el.removeClass('expanded-contant');
+                App.rSideBar.$el.removeClass('expanded');
+                e.target.setAttribute('class' , 'fa fa-bars fa-lg');
+            }
+        },
 
 		/** all post render plugin initialization */
 		initializePlugins: function(){
